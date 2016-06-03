15ead49303c4ddfcdc6d125b57afddae648d01fa
mahmoudhanoraa <mahmoudhanora.mh@gmail.com>
2016-06-03 19:51:45 +0200

    Initial commit

---
+++ app/src/main/java/com/hanora/mahmoud/oht105/MainActivity.java
@@ -0,0 +1,40 @@
+package com.hanora.mahmoud.oht105;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.more_tab_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.feeds:
+                break;
+            case R.id.friends:
+                break;
+            case R.id.about:
+                break;
+        }
+        return true;
+    }
+}
